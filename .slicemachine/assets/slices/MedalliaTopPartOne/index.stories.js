80f4d79ed5e67543f50f76661a7d1910b844cb63
dguardia <david.guardia@gmail.com>
2022-04-03 12:58:29 -0400

    first commit

---
+++ .slicemachine/assets/slices/MedalliaTopPartOne/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MedalliaTopPartOne';
+
+export default {
+  title: 'slices/MedalliaTopPartOne'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"medallia_top_part_one","items":[],"primary":{"title":[{"type":"heading1","text":"Innovate turn-key applications","spans":[]}],"description":[{"type":"paragraph","text":"Ea ullamco et sunt deserunt ullamco sunt sunt.","spans":[]}],"intro":[{"type":"paragraph","text":"Incididunt ipsum duis non ex aute ipsum incididunt pariatur in.","spans":[]}],"numbers":4830},"id":"_Default"}} />
+_Default.storyName = 'Default'
